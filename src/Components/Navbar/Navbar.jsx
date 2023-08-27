76df7d999c284481c04881add24c719ffdbb87db
peach <amoralyse21@gmail.com>
2023-08-27 22:48:02 +0800

    portfolio

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="n-wrapper">
+      <div className="n-left">
+        <div className="n-name">Kishart</div>
+        <span>toggle</span>
+      </div>
+
+      <div className="n-right">
+        <div className="n-list">
+          <ul>
+            <li>Home</li>
+            <li>Services</li>
+            <li>Experiences</li>
+            <li>Portfolio</li>
+            <li>Testimonials</li>
+          </ul>
+        </div>
+        <button className="button">Contact Us</button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
